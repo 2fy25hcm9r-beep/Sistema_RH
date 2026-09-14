1174797cb5b74ed5ab5ee7534927cb5d4c5a7ef2
Ivan Alexander Carrasquedo Vasquez <ivan.carrasquedo@icloud.com>
2026-09-14 09:12:40 -0700

    Corregir MIME del logo WebP

--- servidor/config/urls.py
+++ servidor/config/urls.py
@@ -8,6 +8,8 @@ from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.static import serve
+import mimetypes
+mimetypes.add_type('image/webp', '.webp')
 import os
 
 
